3fc7a64c3be7ccd55a1ce43400b1d1c64b4c7986
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-10-27 12:11:41 -0300

    ajustando variaveis

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
-// frontend/src/api.js
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
+
